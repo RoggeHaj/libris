380f39518217187d2233ac105585b09e5c0f7e8f
Roger Magnesved <roger.magnesved@gmail.com>
2021-01-12 18:37:49 +0100

    Working ISBN search

--- libris.py
+++ libris.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python3
+"""
+Program that retrieves bibliographic information from Libris from a
+given set of search parameters.
+"""
+
+import argparse
+import json
+import urllib.request
+
+def search_libris(args):
+    """ Search Libris using http request """
+
+    _uri = 'http://libris.kb.se/xsearch?query'
+
+    if args.isbn:
+        with urllib.request.urlopen('{u}=isbn:{i}&format=json'.format(u=_uri, i=args.isbn)) as url:
+            data = json.loads(url.read().decode())
+            print(data)
+
+def parse_args():
+    """ Parse the argument list """
+
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('isbn',
+                        type=str,
+                        help='ISBN to search for')
+
+
+    args = parser.parse_args()
+
+    return args
 
 
 def main():
-    print('Main')
+    """ This is the main function """
+
+    args = parse_args()
+
+    search_libris(args)
 
 if __name__ == '__main__':
     main()
