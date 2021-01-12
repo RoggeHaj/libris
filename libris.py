466ead800c9f83e3a48b49f88a0f903d9cba829b
Roger Magnesved <roger.magnesved@gmail.com>
2021-01-12 18:56:18 +0100

    Add example formatting

--- libris.py
+++ libris.py
@@ -15,8 +15,16 @@ def search_libris(args):
 
     if args.isbn:
         with urllib.request.urlopen('{u}=isbn:{i}&format=json'.format(u=_uri, i=args.isbn)) as url:
-            data = json.loads(url.read().decode())
-            print(data)
+            data = json.loads(url.read())
+            book = data['xsearch']['list'][0]
+
+    print('{}\n'.format(json.dumps(data, indent=4, sort_keys=False)))
+
+    print('{a}\n{t}\n{d}\n{i}'.format(
+        a=book['creator'],
+        t=book['title'],
+        d=book['date'],
+        i=book['isbn']))
 
 def parse_args():
     """ Parse the argument list """
